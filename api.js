5070702ec2a4c405d33aae728720e1733a538be5
Chaim Finkelman <chaim@Chaims-MBP.attlocal.net>
2019-03-29 21:51:47 -0700

    add error handlers

--- api.js
+++ api.js
@@ -11,6 +11,22 @@ api.use(bodyParser.json());
 // Routes
 api.use('/users', require('./routes/users'));
 
+// catch Not Found
+api.use((req,res,next)=>{
+  const err = new Error('Not Found');
+  err.status = 404;
+  return next(err);
+});
+
+// Error Handlers
+api.use((err, req, res, next)=>{
+  res.status(err.status || 500);
+  return res.json({
+    message: err.message,
+    error: api.get('env') === 'development' ? err : {}
+  });
+});
+
 // Start Server
 const PORT = process.env.PORT || 3000;
 api.listen(PORT);
