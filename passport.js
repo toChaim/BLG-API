3725b66a7283fcf1d468706c8edec829fcebbcf9
Chaim Finkelman <toChaim@gmail.com>
2019-04-04 19:16:59 -0700

    start adding passport

---
+++ passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+
+const {SECRET} = require('./Const');
+const User = require('./models/user');
+
+passport.use(new JwtStrategy({
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: SECRET
+}, 
+async(payload, done)=>{
+  try{
+    const user = await User.findById(payload.sub);
+    return done(null, user || false);
+  }
+  catch(error){
+    done(error,false);
+  }
+}));
